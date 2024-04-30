db2046e3b7647983cec2ce57ebbe858823cd582b
Shivani692 <164007542+Shivani692@users.noreply.github.com>
2024-04-30 13:01:45 +0530

    shape

---
+++ shapes/s.js
@@ -0,0 +1,9 @@
+var click=document.getElementById("click");
+var reset=document.getElementById("reset");
+var red=document.getElementById("red");
+var blue=document.getElementById("blue");
+function{
+    if(count=>count%5==0){
+        console.log(circle)
+    }
+}
